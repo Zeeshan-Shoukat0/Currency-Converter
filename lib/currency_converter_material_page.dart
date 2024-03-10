683f71a426e40a1946ddccb6d19ce500248225cc
Zeeshan-Shoukat0 <z.eeshanshoukat@hotmail.com>
2024-03-10 14:59:39 +0500

    Changed Text

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -6,7 +6,7 @@ class CurrencyConverter extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Hello'),
+        child: Text('HelloWorld'),
       ),
     );
   }
